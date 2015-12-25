3102eabcb078fa2d30cded093e70e22cec527752
mitjap <mitjap@gmail.com>
2015-12-25 19:44:44 +0100

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+var server = http.createServer();
+server.listen(process.env.PORT || 81);
+
+server.on('listening', function () {
+  console.log("Server listening on port %d in %s mode");
+});
+
+server.on('request', function(req,  res) {
+	res.end(JSON.stringify(req.rawHeaders));
+});
